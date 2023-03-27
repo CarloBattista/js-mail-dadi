8494e715d2c021b9b6f7a6bf44b36cb8b499aacf
CarloBattista <carlitobatti@gmail.com>
2023-03-27 14:12:21 +0200

    generazione numero random per player e computer

--- gioco_dei_dadi/_assets/js/main.js
+++ gioco_dei_dadi/_assets/js/main.js
@@ -5,3 +5,11 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 */
+
+// Genera un numero casuale da 1 a 6 per il player
+let numPlayer = Math.floor(Math.random() * 6) + 1;
+console.log(numPlayer)
+
+// Genera un numero casuale da 1 a 6 per il computer
+let numComputer = Math.floor(Math.random() * 6) + 1;
+console.log(numComputer)
