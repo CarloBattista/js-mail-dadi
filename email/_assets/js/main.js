ddb1b94304018074b7e602e09faf372f13b95a0d
CarloBattista <carlitobatti@gmail.com>
2023-03-27 13:13:16 +0200

    creazine stile html, creazione array email

--- email/_assets/js/main.js
+++ email/_assets/js/main.js
@@ -7,3 +7,11 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 
 */
+
+let email = [
+    "mariorossi@gmail.com", // 0
+    "giusepperossi@gmail.com", // 1
+    "carlorossi@gmail.com", // 2
+    "annarossi@gmail.com", // 3
+    "martinarossi@gmail.com" // 4
+];
